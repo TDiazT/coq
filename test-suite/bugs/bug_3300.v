f76a288274ca87b0428621b05f8c6bc32f287c7d
Tomas <tomdtron@gmail.com>
2025-06-30 20:00:44 +0200

    feat: Allow prim projections with postponed eta conversion check
    fix: postponed eta conv flag not set in one case

--- test-suite/bugs/bug_3300.v
+++ test-suite/bugs/bug_3300.v
@@ -1,7 +1,8 @@
 Set Primitive Projections.
-Record Box (T : Type) : Prop := wrap {prop : T}.
-
-Definition down (x : Type) : Prop := Box x.
+Fail Record Box (T : Type) : Prop := wrap {prop : T}.
+(* The quality constraints are inconsistent: cannot enforce Prop -> Type because it would identify Type and Prop which is inconsistent. 
+    This is introduced by the constraints Prop -> Type *)
+Fail Definition down (x : Type) : Prop := Box x.
 Definition up (x : Prop) : Type := x.
 
 Fail Definition back A : up (down A) -> A := @prop A.
